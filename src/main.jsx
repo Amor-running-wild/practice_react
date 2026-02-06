8b40483279ccf803cd9d45e23ba8567eeaffc1f8
Amor Zephyr <amorzephyr@gmail.com>
2026-02-06 12:15:32 +0500

    Create routing directory and write along react routing lesson of Odin project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import FunctionalInput from './Todo'
-import ClassInput from './TodoClass'
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import routes from "./routing/Routes";
+
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClassInput name="Just Do It"></ClassInput>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
